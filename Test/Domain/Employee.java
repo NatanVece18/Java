611037de4858ee5a2bd2e3061458ab65ee2a95b4
Natan Santos <natan18vece@gmail.com>
2026-04-12 22:19:49 -0300

    Create Employee.java

---
+++ Test/Domain/Employee.java
@@ -0,0 +1,32 @@
+package academy.MyTests.tests2.domain;
+
+public class Employee {
+    private String name;
+    private double salary;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public void report(){
+        System.out.println("Name: " + this.name);
+        System.out.println("Salary: " + this.salary);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
